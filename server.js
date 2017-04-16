ba5380987d85a729b47910eda354f7d3638f6152
git-clay <ce.smith5525@gmail.com>
2017-04-15 21:31:44 -0600

    random buttons and testing tags

--- server.js
+++ server.js
@@ -149,12 +149,12 @@ slapp.command('/getSnap', (msg, text)=>{
         .route('getid1', state)
     }
 
-    // add their response to state
-    state.status = text
     var host;
     if(randSnap){
+      state.status = randSnap
       host = "https://api-cms-fitrock.kinetise.com/api/kinetise/v2/projects/199a5286a75bd6a4bddd37c6c62ee310/tables/1/rows?id="+text+"&access_token=NGU1MzYxYTA1NGNlZDk2NjdlYzQ0OGU4N2Y3M2E5NTNhM2I2NTY0OThkODU5YjVmZDZjMjhmZjY1ZDI5OGFjZg"
     } else {
+      state.status = randTag
       host = "https://api-cms-fitrock.kinetise.com/api/kinetise/v2/projects/199a5286a75bd6a4bddd37c6c62ee310/tables/5/rows?id="+text+"&access_token=NGU1MzYxYTA1NGNlZDk2NjdlYzQ0OGU4N2Y3M2E5NTNhM2I2NTY0OThkODU5YjVmZDZjMjhmZjY1ZDI5OGFjZg"
     }
 
