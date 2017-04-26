c03172b6eb0a89fd61494bcb0dcdd1c28ba68c0e
git-clay <ce.smith5525@gmail.com>
2017-04-26 10:06:23 -0600

    moved msg.say

--- slashCommands/testSlash.js
+++ slashCommands/testSlash.js
@@ -53,39 +53,55 @@ let test = function() {
         .route('requestToDatabase', state)
     }
     let body
+    /*
+      function to get client ip and convert to geolocation goes here
+
+    */
     let radius = getRadius(39.752764, -104.877743) //test: snap #1055
     
 
-// Get client IP address from request object ----------------------
-getClientAddress = function (req) {
-  console.log(req.connection.remoteAddress)
-        return (req.headers['x-forwarded-for'] || '').split(',')[0] 
-        || req.connection.remoteAddress;
-};
-console.log(getClientAddress())
-
-//firebase search by snap lat (start at bottom of circle, end at top)
+    //firebase search by snap lat (start at bottom of circle, end at top)
     let snapLat = snaps
       .orderByChild('lat')
       .startAt(radius[5].lat + "-") // "-"makes a string: required for query
       .endAt(radius[1].lat + "-")
       .once('value')
       .then(function(snap) {
-        snap.forEach(function(data) {
-          //if returns lng within radius (east/west)
-          if (data.val().lng <= radius[0].lng && data.val().lng >= radius[3].lng) {
-            console.log(data.val().title)
-            let body = data.val()
-            msg.say(```
-              I found a deal for you: 
-              ${body.description}\n
-              ${body.picture}\n
-              ${body.address}\n
-              ```)
-          }
-        })
-      })
-    })
+        msg.say(
+                // `I found a deal for you: 
+                // ${body.description}\n
+                // ${body.picture}\n
+                // ${body.address}\n`
+          snap.forEach(function(data) {
+            //if returns lng within radius (east/west)
+            if (data.val().lng <= radius[0].lng && data.val().lng >= radius[3].lng) {
+              // console.log(data.val().title)
+              let body = data.val()
+
+                attachments: [{
+                text: `I found some deals for you: `,
+                // fallback: 'Where to today?',
+                // callback_id: 'doit_confirm_callback',
+                actions: [{
+                    name: 'answer',
+                    text: ``,
+                    type: 'text',
+                    value: randomNum
+                    },
+                    {
+                      name: 'answer',
+                      text: 'copy of button',
+                      type: 'button',
+                      value: randomNum
+                    }
+                  ]
+                }] //end attachments
+            } //end if (lng checker)
+          }) //end foreach
+        ) //end msg.say
+
+      }) //end .then(snap)
+    }) //end .route('requestToDatabase')
 }
 module.exports = {
   test:test()
