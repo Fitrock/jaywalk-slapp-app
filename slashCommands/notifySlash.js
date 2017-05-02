f0d3bd7c8002d5a3b7528a2363cc591a7b9e7c74
git-clay <ce.smith5525@gmail.com>
2017-05-02 14:37:28 -0600

    auth header

--- slashCommands/notifySlash.js
+++ slashCommands/notifySlash.js
@@ -88,7 +88,9 @@ let notify  = function() {
               "two": 2
           }
         },
-        "Authorization": `Bearer ${process.env.BEEPBOOP_TOKEN}`,       
+        "headers":{
+          "Authorization": `Bearer ${process.env.BEEPBOOP_TOKEN}`
+        },       
         "schedule": "0 * * * * *",
           "expires": "2017-06-01T23:00:00Z"
       },callback);
