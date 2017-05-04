68900dd4a6028077dddc149ca2c062e094ccf95e
git-clay <ce.smith5525@gmail.com>
2017-05-03 21:05:06 -0600

    narrowed it down to slapp.context

--- slackSetup.js
+++ slackSetup.js
@@ -4,15 +4,11 @@ const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
 
-console.log(process.env.SLACK_VERIFY_TOKEN)
-console.log(process.env.VERIFY_TOKEN)
-
 const slapp = Slapp({
   // Beep Boop sets the SLACK_VERIFY_TOKEN env var
   verify_token: process.env.SLACK_VERIFY_TOKEN,
   convo_store: ConvoStore(),
-  context: Context(),
-  ignoreSelf: false
+  context: Context()
 })
 module.exports = {
 	slapp: slapp
