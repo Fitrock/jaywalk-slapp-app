4859e5fc67fec942c7cb007def8ea004f7a8d669
git-clay <ce.smith5525@gmail.com>
2017-05-04 20:55:25 -0600

    logs

--- slashCommands/routes/snapsByGeoRoute.js
+++ slashCommands/routes/snapsByGeoRoute.js
@@ -25,7 +25,7 @@ function snapsByGeo (lat,lng, msg, state){
         let body
         let count = 0
         let len = snaps.length
-        console.log(len)
+        console.log(snaps)
         for(let i=len;i>=len-4;i--){
           snap = snaps[i].val()
           console.log(snap)
