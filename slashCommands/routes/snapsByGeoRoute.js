6bc6b030c0b82a2d3510ac4dbaae96abbcf06954
git-clay <ce.smith5525@gmail.com>
2017-05-04 23:31:41 -0600

    asdf

--- slashCommands/routes/snapsByGeoRoute.js
+++ slashCommands/routes/snapsByGeoRoute.js
@@ -14,7 +14,8 @@ const firebase    = require('../../firebaseSetup.js'),
 
 function snapsByGeo (lat,lng, msg, state){
     //firebase search by snap lat (start at bottom of circle, end at top)
-    let radius = getRadius(lat,lng) //test: snap #1055
+    let radius = getRadius(lat,lng)
+    let resultArr = []
     let snapLat = snaps
       .orderByChild('lat')
       .startAt(radius[5].lat + "-") // "-"makes a string: required for query
@@ -24,36 +25,31 @@ function snapsByGeo (lat,lng, msg, state){
         let count = 0
         snapObj.forEach(function(data) {
           let snap = data.val()
-          // console.log(snap)
           //if returns lng within radius (east/west)
-            //  console.log(snap.lng,'>',radius[0].lng,snap.lat)
-            // console.log(snap.lng,'<',radius[3].lng)
-            console.log(count)
-            count++
           if (snap.lng <= radius[0].lng && snap.lng >= radius[3].lng) {
-            console.log(snap.lng,'<',radius[0].lng)
-            console.log(snap.lng,'>',radius[3].lng)
             count ++
-            let thisCount = count
-            let callback = function(picUrl){
-              msg.say({
-                  text: '',
-                  attachments:[{
-                    title: `${snap.description}`,
-                    color: 'warning',
-                    image_url: `${picUrl}`,
-                    thumb_url: `${picUrl}`,
-                    text: `${snap.address}`,
-                    footer:`Jaywalk: ${thisCount}`
-                  }]
-              }) //end msg.say
-              // .route('relaventAsk', (msg,state),60)
-            }
-            tinyurl.shorten(snap.picture, function(res) {
-              // callback(res)
-            })
+            resultArr.push(snap)
           } //end if (lng checker)
         }) //end foreach
+
+        for(let i=count;i>(count-4);i--){ //last four
+          let callback = function(picUrl){
+            msg.say({
+                text: '',
+                attachments:[{
+                  title: `${snap.description}`,
+                  color: 'warning',
+                  image_url: `${picUrl}`,
+                  text: `${snap.address}`
+                  // footer:`Jaywalk: ${thisCount}`
+                }]
+            }) //end msg.say
+            // .route('relaventAsk', (msg,state),60)
+          }
+          tinyurl.shorten(snap.picture, function(res) {
+            // callback(res)
+          })
+        } //end for
       }) //end .then(snap)
 } // end snapsByGeo()
 
