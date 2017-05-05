ba59abd183714ae18036a20897f28bb11f1ff8c1
git-clay <ce.smith5525@gmail.com>
2017-05-04 22:01:05 -0600

    kjsdjd

--- slashCommands/routes/snapsByGeoRoute.js
+++ slashCommands/routes/snapsByGeoRoute.js
@@ -18,9 +18,9 @@ function snapsByGeo (lat,lng, msg, state){
 
     let snapLat = snaps
       .orderByChild('snap_id')
+      .limitToLast('4')
       .startAt(radius[5].lat + "-") // "-"makes a string: required for query
       .endAt(radius[1].lat + "-")
-      .limitToLast('4')
       .once('value')
       .then(function(snapObj) {
         let body
