aa41f36ab103b833c77af60285991e86e5277e15
git-clay <ce.smith5525@gmail.com>
2017-05-04 22:15:36 -0600

    asdf

--- slashCommands/testSlash.js
+++ slashCommands/testSlash.js
@@ -177,7 +177,7 @@ let test = function() {
         //   msg.respond(msg.body.response_url, `${value} is a good choice!`)
         // })
         // if(msg.body.actions[0].value=="yes"){
-          console.log(teamInfo)
+          // console.log(teamInfo)
           snapsByGeo(teamInfo.lat,teamInfo.lng, msg, state) 
         // } else {
         //   msg.say('still working on that')
