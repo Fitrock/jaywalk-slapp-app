23a631bf7337b995e0904be7d1881d983d804cc2
git-clay <ce.smith5525@gmail.com>
2017-05-09 15:46:49 -0600

    team settup and team settings js init

--- newTeamEvent.js
+++ newTeamEvent.js
@@ -3,4 +3,5 @@
 	asks to set company name and location
 	stores teamInfo to firebase
 */
+const slapp       = require('./slackSetup.js').slapp
 const firebase    = require('./firebaseSetup.js')
--- teamSettings.js
+++ teamSettings.js
@@ -1,3 +1,5 @@
 /*
 	used to setup/update company name, address, notifications...
 */
+const slapp       = require('./slackSetup.js').slapp
+const firebase    = require('./firebaseSetup.js')
