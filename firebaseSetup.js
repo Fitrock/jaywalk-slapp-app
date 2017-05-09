b4d1f4a0c39365c1e932ecc7912caa8fa82f90f0
git-clay <ce.smith5525@gmail.com>
2017-05-09 12:25:25 -0600

    yml

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -84,7 +84,7 @@ let bars = {
   team_id:"",
   incoming_webhook:""
 }
-console.log(process.env.fire_email)
+console.log(process.env.FIRE_EMAIL)
 
 // firebase.auth().signInWithEmailAndPassword(process.env.fire_email, process.env.fire_pass).catch(function(error) {
 //   // Handle Errors here.
