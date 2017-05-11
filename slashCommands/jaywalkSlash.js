e929a9eaff8b1ed78051f3564e8594877de1772e
git-clay <ce.smith5525@gmail.com>
2017-05-11 11:02:36 -0600

    testing route to start

--- slashCommands/jaywalkSlash.js
+++ slashCommands/jaywalkSlash.js
@@ -59,6 +59,7 @@ const jaywalk = function() {
 
 
   slapp.command('/jaywalk', (msg, text) => {
+    slapp.route('relaventAsk', state,60)
     teamInfo = slackDb
       .child(msg.body.team_id)
       .once("value")
